43f5f022f0b9f82d65bfe915f3431234fb51105b
Daniel12540 <97809701+Daniel12540@users.noreply.github.com>
2026-07-29 22:54:29 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,14 +1,12 @@
-JavaScript
-
 const express = require('express');
 const axios = require('axios');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 const STATIONS = {
-    "eska": "https://stream.open.fm/321",
-    "rmf": "https://rs102-krk.rmftv.pl/RMFFM48",
-    "lofi": "https://stream.zeno.fm/f3wvbbqmdg8uv"
+    "eska": "[https://stream.open.fm/321](https://stream.open.fm/321)",
+    "rmf": "[https://rs102-krk.rmftv.pl/RMFFM48](https://rs102-krk.rmftv.pl/RMFFM48)",
+    "lofi": "[https://stream.zeno.fm/f3wvbbqmdg8uv](https://stream.zeno.fm/f3wvbbqmdg8uv)"
 };
 
 app.use((req, res, next) => {
